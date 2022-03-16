08823f9aa98ed198ab3a686ba21405e7e0a6b60b
Robert B <robertb@lifesciencepublishers.com>
2022-03-15 18:16:16 -0600

    Add convert json functionality for both YML and CSV

--- backend/index.php
+++ backend/index.php
@@ -1,37 +1,57 @@
 <?php
-$addr = array(
-    "given" => "Chris",
-    "family"=> "Dumars",
-    "address"=> array(
-        "lines"=> "458 Walkman Dr.
-        Suite #292",
-        "city"=> "Royal Oak",
-        "state"=> "MI",
-        "postal"=> 48046,
-      ),
-  );
-$invoice = array (
-    "invoice"=> 34843,
-    "date"=> 980208000,
-    "bill-to"=> $addr,
-    "ship-to"=> $addr,
-    "product"=> array(
-        array(
-            "sku"=> "BL394D",
-            "quantity"=> 4,
-            "description"=> "Basketball",
-            "price"=> 450,
-          ),
-        array(
-            "sku"=> "BL4438H",
-            "quantity"=> 1,
-            "description"=> "Super Hoop",
-            "price"=> 2392,
-          ),
-      ),
-    "tax"=> 251.42,
-    "total"=> 4443.52,
-    "comments"=> "Late afternoon is best. Backup contact is Nancy Billsmer @ 338-4338.",
-  );
-var_dump(yaml_emit($invoice));
+    function setYmlFromJson(){
+        $strJsonFileContents = file_get_contents("./data/spacex.json");
+        $jasonArray = json_decode($strJsonFileContents, true);
+        $ymlText = yaml_emit($jasonArray);
+        $file = fopen('./data/spacex.yml', 'w') or die("Can't create file");
+        fwrite($file, $ymlText);
+        fclose($file);
+    }
+
+    function setCsvFromJson(){
+        $file = "./data/spacex.json";
+        $jsonContents = file_get_contents($file);
+        $arrayContents = json_decode($jsonContents, true);
+        $fcsv = fopen('./data/spacex.csv', 'w');
+        $launchArray = [];
+        foreach($arrayContents['data']['launches'] as $arr){
+            $newArray = array(
+                'id' => $arr['id'],
+                'launch_date_local' => $arr['launch_date_local'],
+                'site_name' => $arr['launch_site']['site_name'],
+                'site_name_long' => $arr['launch_site']['site_name_long'],
+                'launch_year' => $arr['launch_year'],
+                'article_link' => $arr['links']['article_link'],
+                'flickr_images' => implode(',', $arr['links']['flickr_images']),
+                'mission_id' => implode(',', $arr['mission_id']),
+                'mission_name' => $arr['mission_name'],
+                'rocket_name' => $arr['rocket']['rocket_name'],
+                'rocket_type' => $arr['rocket']['rocket_type']
+            );
+            $launchArray[] = $newArray;
+        }
+        $csv = '';
+        $header = false;
+        foreach ($launchArray as $line) {	
+            if (empty($header)) {
+                $header = array_keys($line);
+                fputcsv($fcsv, $header);
+                $csv .= implode(',', $header);
+                $header = array_flip($header);		
+            }
+            
+            $line_array = array();
+            
+            foreach($line as $value) {
+                array_push($line_array, $value);
+            }
+            
+            $csv .= "\n" . implode(',', $line_array);
+        
+            fputcsv($fcsv, $line_array);
+        }
+        fclose($fcsv);
+    }
+
+    setCsvFromJson();
 ?>
